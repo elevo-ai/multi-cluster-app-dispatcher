bb9ce3d70814f1abfc53049522add7cf8e30c64e
Anuradha Hegde <12959032+hegdean@users.noreply.github.com>
2024-03-12 11:04:36 -0700

    Updated build script (#2)
    Co-authored-by: Anuradha Hegde <anuradhah@aizencorp.com>

--- build_foresight_mcad.sh
+++ build_foresight_mcad.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
 mcad_image=mcad-controller
-mcad_imagetag=main-v1.38.1
-aizen_release=0.0.1
+mcad_imagetag=0.0.1
 if [[ -z ${AIZENREPO} ]]; then
    aizen_repo=172.16.26.41:5000
 else
    aizen_repo=$AIZENREPO
 fi
 #Cleanup the image
-make clean
 echo "$(docker images -q $mcad_image:$mcad_imagetag 2> /dev/null)"
 if [[ ! -z "$(docker images -q $mcad_image:$mcad_imagetag 2> /dev/null)" ]]; then
-   echo "docker image rm $mcad_image:$mcad_imagetag $aizen_repo/foresight-mcad-controller:$aizen_release"
-   docker image rm $mcad_image:$mcad_imagetag $aizen_repo/foresight-mcad-controller:$aizen_release
-   echo "docker image rm $(docker images -q $mcad_image:$mcad_imagetag)"
-   docker image rm $(docker images -q $mcad_image:$mcad_imagetag)
+   docker image rm $aizen_repo/foresight-mcad-controller:$mcad_imagetag
+   docker image rm $mcad_image:$mcad_imagetag
 fi
-exit 0
 make mcad-controller
-if [[ -e  _output/bin/"mcad_controller" ]]; then
+if [[ -e  _output/bin/"mcad-controller" ]]; then
+   echo -e "\n**** Proceeding to build mcad images *****\n"
    make images
    if [ ! -z "$(docker images -q $mcad_image:$mcad_imagetag 2> /dev/null)" ]; then
-    docker tag $mcad_image:$mcad_imagetag $aizen_repo/foresight-mcad-controller:$aizen_release
-    echo -e "\nSuccessfully pushed image $aizen_repo/foresight-mcad-controller:$aizen_release\n"
+    docker tag $mcad_image:$mcad_imagetag $aizen_repo/foresight-mcad-controller:$mcad_imagetag
+    docker push $aizen_repo/foresight-mcad-controller:$mcad_imagetag
+    echo -e "\nSuccessfully pushed image $aizen_repo/foresight-mcad-controller:$mcad_imagetag\n"
+    docker image rm $aizen_repo/foresight-mcad-controller:$mcad_imagetag
+    docker image rm $mcad_image:$mcad_imagetag
     exit 0
    fi
 else
